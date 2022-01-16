74c8b959e84515f06b859a9051631787b2c4ee77
Josimar Nepomuceno <josimar.n2007@gmail.com>
2022-01-16 16:04:59 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,18 +1,28 @@
 import axios from "axios";
+import { useState, useEffect } from "react";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/pagination";
 import { BASE_URL } from "utils/requests";
+import { MoviePage } from "types/movie";
+
 
 function Listing(){
 
-    // Forma Errada
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    const [pageNumber, stePageNumber] = useState(0);
+
+    useEffect(()=>{
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
         .then(response => {
+            const data = response.data as MoviePage;
             console.log(response.data);
+            stePageNumber(data.number);
         });
+    }, []);
+   
 
     return(
         <>
+            <p>{pageNumber}</p>
             <Pagination/>
 
             <div className="container">
